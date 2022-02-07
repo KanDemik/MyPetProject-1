e6ba09943672d631fe32a6eda6d2f2e31fc6db97
KanDemik <85833523+KanDemik@users.noreply.github.com>
2022-02-07 20:19:02 +0700

    Add files via upload

---
+++ js/filterSelect.js
@@ -0,0 +1,242 @@
+let filterEraseBtn = document.querySelector('.filters__erase-btn')
+let filterSelectBtn1 = document.querySelector('#filterSelectBtn1');
+let filterSelectBtn2 = document.querySelector('#filterSelectBtn2');
+let filterSelectBtn3 = document.querySelector('#filterSelectBtn3');
+let filterSelectBtn4 = document.querySelector('#filterSelectBtn4');
+let filterSelectBtn5 = document.querySelector('#filterSelectBtn5');
+let filterSelectBtn6 = document.querySelector('#filterSelectBtn6');
+let filterSelectBtn7 = document.querySelector('#filterSelectBtn7');
+let filterSelectBtn8 = document.querySelector('#filterSelectBtn8');
+let filterSelectBtn9 = document.querySelector('#filterSelectBtn9');
+let filterSelectBtn10 = document.querySelector('#filterSelectBtn10');
+let filterSelectBtn11 = document.querySelector('#filterSelectBtn11');
+let filterSelectBtn12 = document.querySelector('#filterSelectBtn12');
+let filterSelectBtn13 = document.querySelector('#filterSelectBtn13');
+let filterSelectBtn14 = document.querySelector('#filterSelectBtn14');
+let filterSelectBtn15 = document.querySelector('#filterSelectBtn15');
+let filterSelectBtn16 = document.querySelector('#filterSelectBtn16');
+let filterSelectBtn17 = document.querySelector('#filterSelectBtn17');
+let filterSelectBtn18 = document.querySelector('#filterSelectBtn18');
+let filterSelectBtn19 = document.querySelector('#filterSelectBtn19');
+let filterSelectBtn20 = document.querySelector('#filterSelectBtn20');
+let filterSelectBtn21 = document.querySelector('#filterSelectBtn21');
+let filterSelectBtn22 = document.querySelector('#filterSelectBtn22');
+let filterSelectBtn23 = document.querySelector('#filterSelectBtn23');
+let filterSelectBtn24 = document.querySelector('#filterSelectBtn24');
+let filterSelectBtn25 = document.querySelector('#filterSelectBtn25');
+let filterSelectBtn26 = document.querySelector('#filterSelectBtn26');
+let filterSelectBtn27 = document.querySelector('#filterSelectBtn27');
+let filterSelectBtn28 = document.querySelector('#filterSelectBtn28');
+let filterSelectBtn29 = document.querySelector('#filterSelectBtn29');
+let filterSelectBtn30 = document.querySelector('#filterSelectBtn30');
+let filterSelectBtn31 = document.querySelector('#filterSelectBtn31');
+let filterSelectBtn32 = document.querySelector('#filterSelectBtn32');
+let filterSelectBtn33 = document.querySelector('#filterSelectBtn33');
+let filterSelectBtn34 = document.querySelector('#filterSelectBtn34');
+let filterSelectBtn35 = document.querySelector('#filterSelectBtn35');
+let filterSelectBtn36 = document.querySelector('#filterSelectBtn36');
+let filterSelectBtn37 = document.querySelector('#filterSelectBtn37');
+
+filterSelectBtn1.addEventListener('click', function() {
+    filterSelectBtn1.classList.add('selected')
+})
+
+filterSelectBtn2.addEventListener('click', function() {
+    filterSelectBtn2.classList.add('selected')
+})
+
+
+filterSelectBtn3.addEventListener('click', function() {
+    filterSelectBtn3.classList.add('selected')
+})
+
+filterSelectBtn4.addEventListener('click', function() {
+    filterSelectBtn4.classList.add('selected')
+})
+
+filterSelectBtn5.addEventListener('click', function() {
+    filterSelectBtn5.classList.add('selected')
+})
+
+filterSelectBtn6.addEventListener('click', function() {
+    filterSelectBtn6.classList.add('selected')
+})
+
+filterSelectBtn7.addEventListener('click', function() {
+    filterSelectBtn7.classList.add('selected')
+})
+
+filterSelectBtn8.addEventListener('click', function() {
+    filterSelectBtn8.classList.add('selected')
+})
+
+filterSelectBtn9.addEventListener('click', function() {
+    filterSelectBtn9.classList.add('selected')
+})
+
+filterSelectBtn10.addEventListener('click', function() {
+    filterSelectBtn10.classList.add('selected')
+})
+
+filterSelectBtn11.addEventListener('click', function() {
+    filterSelectBtn11.classList.add('selected')
+})
+
+filterSelectBtn12.addEventListener('click', function() {
+    filterSelectBtn12.classList.add('selected')
+})
+
+filterSelectBtn13.addEventListener('click', function() {
+    filterSelectBtn13.classList.add('selected')
+})
+
+filterSelectBtn14.addEventListener('click', function() {
+    filterSelectBtn14.classList.add('selected')
+})
+
+filterSelectBtn15.addEventListener('click', function() {
+    filterSelectBtn15.classList.add('selected')
+})
+
+filterSelectBtn16.addEventListener('click', function() {
+    filterSelectBtn16.classList.add('selected')
+})
+
+filterSelectBtn17.addEventListener('click', function() {
+    filterSelectBtn17.classList.add('selected')
+})
+
+filterSelectBtn18.addEventListener('click', function() {
+    filterSelectBtn18.classList.add('selected')
+})
+
+filterSelectBtn19.addEventListener('click', function() {
+    filterSelectBtn19.classList.add('selected')
+})
+
+filterSelectBtn20.addEventListener('click', function() {
+    filterSelectBtn20.classList.add('selected')
+})
+
+filterSelectBtn21.addEventListener('click', function() {
+    filterSelectBtn21.classList.add('selected')
+})
+
+filterSelectBtn21.addEventListener('click', function() {
+    filterSelectBtn21.classList.add('selected')
+})
+
+filterSelectBtn22.addEventListener('click', function() {
+    filterSelectBtn22.classList.add('selected')
+})
+
+filterSelectBtn23.addEventListener('click', function() {
+    filterSelectBtn23.classList.add('selected')
+})
+
+filterSelectBtn24.addEventListener('click', function() {
+    filterSelectBtn24.classList.add('selected')
+})
+
+
+filterSelectBtn25.addEventListener('click', function() {
+    filterSelectBtn25.classList.add('selected')
+})
+
+
+filterSelectBtn26.addEventListener('click', function() {
+    filterSelectBtn26.classList.add('selected')
+})
+
+
+filterSelectBtn27.addEventListener('click', function() {
+    filterSelectBtn27.classList.add('selected')
+})
+
+
+filterSelectBtn28.addEventListener('click', function() {
+    filterSelectBtn28.classList.add('selected')
+})
+
+
+filterSelectBtn29.addEventListener('click', function() {
+    filterSelectBtn29.classList.add('selected')
+})
+
+
+filterSelectBtn30.addEventListener('click', function() {
+    filterSelectBtn30.classList.add('selected')
+})
+
+
+filterSelectBtn31.addEventListener('click', function() {
+    filterSelectBtn31.classList.add('selected')
+})
+
+filterSelectBtn32.addEventListener('click', function() {
+    filterSelectBtn32.classList.add('selected')
+})
+
+filterSelectBtn33.addEventListener('click', function() {
+    filterSelectBtn33.classList.add('selected')
+})
+
+filterSelectBtn34.addEventListener('click', function() {
+    filterSelectBtn34.classList.add('selected')
+})
+
+filterSelectBtn35.addEventListener('click', function() {
+    filterSelectBtn35.classList.add('selected')
+})
+
+
+filterSelectBtn36.addEventListener('click', function() {
+    filterSelectBtn36.classList.add('selected')
+})
+
+
+filterSelectBtn37.addEventListener('click', function() {
+    filterSelectBtn37.classList.add('selected')
+})
+
+
+filterEraseBtn.addEventListener('click', function() {
+    filterSelectBtn1.classList.remove('selected')
+    filterSelectBtn2.classList.remove('selected')
+    filterSelectBtn3.classList.remove('selected')
+    filterSelectBtn4.classList.remove('selected')
+    filterSelectBtn5.classList.remove('selected')
+    filterSelectBtn6.classList.remove('selected')
+    filterSelectBtn7.classList.remove('selected')
+    filterSelectBtn8.classList.remove('selected')
+    filterSelectBtn9.classList.remove('selected')
+    filterSelectBtn10.classList.remove('selected')
+    filterSelectBtn11.classList.remove('selected')
+    filterSelectBtn12.classList.remove('selected')
+    filterSelectBtn13.classList.remove('selected')
+    filterSelectBtn14.classList.remove('selected')
+    filterSelectBtn15.classList.remove('selected')
+    filterSelectBtn16.classList.remove('selected')
+    filterSelectBtn17.classList.remove('selected')
+    filterSelectBtn18.classList.remove('selected')
+    filterSelectBtn19.classList.remove('selected')
+    filterSelectBtn20.classList.remove('selected')
+    filterSelectBtn21.classList.remove('selected')
+    filterSelectBtn22.classList.remove('selected')
+    filterSelectBtn23.classList.remove('selected')
+    filterSelectBtn24.classList.remove('selected')
+    filterSelectBtn25.classList.remove('selected')
+    filterSelectBtn26.classList.remove('selected')
+    filterSelectBtn27.classList.remove('selected')
+    filterSelectBtn28.classList.remove('selected')
+    filterSelectBtn29.classList.remove('selected')
+    filterSelectBtn30.classList.remove('selected')
+    filterSelectBtn31.classList.remove('selected')
+    filterSelectBtn32.classList.remove('selected')
+    filterSelectBtn33.classList.remove('selected')
+    filterSelectBtn34.classList.remove('selected')
+    filterSelectBtn35.classList.remove('selected')
+    filterSelectBtn36.classList.remove('selected')
+    filterSelectBtn37.classList.remove('selected')
+})
+
--- js/script.js
+++ js/script.js
@@ -3,6 +3,12 @@ let headerNavMobile = document.querySelector('.header__mobile-nav');
 let deliveryFullText = document.querySelector('.delivery__fulltext');
 let deliveryFog = document.querySelector('.delivery__fog');
 let deliveryBody = document.querySelector('.delivery__body');
+let filtersCross = document.querySelector('.filters__cross');
+let filter = document.querySelector('.filters');
+let productWindowCross = document.querySelector('.product-window__cross');
+let productWindowCard = document.querySelector('.product-window__card');
+let productWindow = document.querySelector('.product-window')
+let dom = document;
 var doc, bod, htm;
 
 addEventListener('load', function(){
@@ -17,10 +23,46 @@ addEventListener('load', function(){
 headerBurger.addEventListener('click', function() {
     headerBurger.classList.toggle('active');
     headerNavMobile.classList.toggle('active');
-})
+});
 
 deliveryFullText.addEventListener('click', function() {
     deliveryFullText.classList.toggle('active');
     deliveryFog.classList.toggle('appear');
     deliveryBody.classList.toggle('active');
+});
+
+document.addEventListener('DOMContentLoaded', function(){
+    const filtersBtn = document.querySelectorAll('.filters-btn');
+    filtersBtn.forEach(b => b.addEventListener('click', function(){
+        filter.classList.add('active')
+    }));
+});
+
+filtersCross.addEventListener('click', function() {
+    filter.classList.remove('active');
+});
+
+productWindowCross.addEventListener('click', function() {
+    productWindowCard.classList.remove('active')
+    productWindow.classList.remove('active')
+})
+
+document.addEventListener('DOMContentLoaded', function(){
+    const productBtn = document.querySelectorAll('.product__pizza .product__btn');
+    productBtn.forEach(b => b.addEventListener('click', function(){
+        productWindow.classList.add('active')
+        productWindowCard.classList.add('active')
+    }));
+});
+
+let headerShop = document.querySelector('.header__shop');
+let shoppingCart = document.querySelector('.shopping-cart');
+let shoppingCartCross = document.querySelector('.shopping-cart__cross');
+
+shoppingCartCross.addEventListener('click', function() {
+    shoppingCart.classList.remove('active') 
+})
+
+headerShop.addEventListener('click', function() {
+    shoppingCart.classList.add('active')    
 })
